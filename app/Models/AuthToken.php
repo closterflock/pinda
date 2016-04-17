c08adfb717d0297a59def5239683d0af87dbc123
James Spence <Jamesaspence20@gmail.com>
2016-04-17 17:30:24 -0500

    guarded / hidden fields on AuthToken

--- app/Models/AuthToken.php
+++ app/Models/AuthToken.php
@@ -9,6 +9,9 @@ namespace App\Models;
  */
 class AuthToken extends AbstractModel
 {
+    public $guarded = ['id'];
+
+    protected $hidden = ['id', 'created_at', 'updated_at', 'user', 'user_id', 'user_agent'];
 
     public function user()
     {
