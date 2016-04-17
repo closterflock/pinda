07af7a796915d9901babd96f0b8e0205aa43df7d
James Spence <Jamesaspence20@gmail.com>
2016-04-17 14:51:59 -0500

    VerifyAPIToken middleware

---
+++ app/Http/Middleware/VerifyAPIToken.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace App\Http\Middleware;
+
+
+use App\Http\Response\APIResponseFactory;
+use App\Models\AuthToken;
+use App\Models\Repository\ModelRepository;
+use Closure;
+use Illuminate\Auth\AuthManager;
+
+class VerifyAPIToken
+{
+    /**
+     * @var ModelRepository
+     */
+    private $repository;
+    /**
+     * @var APIResponseFactory
+     */
+    private $factory;
+    /**
+     * @var AuthManager
+     */
+    private $auth;
+
+    public function __construct(ModelRepository $repository, APIResponseFactory $factory, AuthManager $auth)
+    {
+        $repository->setModel(AuthToken::class);
+        $this->repository = $repository;
+        $this->factory = $factory;
+        $this->auth = $auth;
+    }
+
+    /**
+     * Creates an unauthorized response.
+     *
+     * @param string $status
+     * @param string $message
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function createUnauthorizedResponse($status = 'invalid_auth', $message = 'Invalid auth token')
+    {
+        return $this->factory->make($status, $message, [], 401);
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     * @internal param null|string $guard
+     */
+    public function handle($request, Closure $next)
+    {
+        $apiToken = $request->headers->get('auth_token');
+        $authId = $request->headers->get('auth_id');
+
+        if (is_null($apiToken) || is_null($authId)) {
+            return $this->createUnauthorizedResponse('missing_auth', 'Missing either auth id or auth token');
+        }
+
+        /** @var AuthToken $authToken */
+        $authToken = $this
+            ->repository
+            ->with('user')
+            ->where('token', '=', $apiToken)
+            ->where('id', '=', $authId)
+            ->first();
+
+        if (!isset($authToken)) {
+            return $this->createUnauthorizedResponse();
+        }
+
+        $this->auth->guard()->setUser($authToken->user);
+
+        return $next($request);
+    }
+
+}
