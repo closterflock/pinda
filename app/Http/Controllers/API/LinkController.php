128ca4702559efbcf277fd1fca49fb7f24c2440b
James Spence <Jamesaspence20@gmail.com>
2016-04-19 20:06:50 -0500

    rewrote api, new methods for update / get single link

--- app/Http/Controllers/API/LinkController.php
+++ app/Http/Controllers/API/LinkController.php
@@ -4,32 +4,64 @@
 namespace App\Http\Controllers\API;
 
 
+use App\Http\Response\APIResponseFactory;
+use App\Models\Factory\ModelFactory;
 use App\Models\Link;
 use App\Models\Repository\LinkRepository;
-use App\Services\LinkService;
 use Illuminate\Http\Request;
 
 class LinkController extends APIController
 {
+    /**
+     * @var LinkRepository
+     */
+    private $repository;
+
+    public function __construct(APIResponseFactory $responseFactory, LinkRepository $repository)
+    {
+        parent::__construct($responseFactory);
+        $this->repository = $repository;
+    }
 
     /**
      * Retrieves a list of links.
      *
+     * @route /api/v1/links
      * @method GET
      * @param Request $request
-     * @param LinkRepository $repository
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
      */
-    public function getLinks(Request $request, LinkRepository $repository)
+    public function getLinks(Request $request)
     {
         return $this->successResponse('Success', [
-            'links' => $repository->getLinksForUser($request->user())
+            'links' => $this->repository->getLinksForUser($request->user())
+        ]);
+    }
+
+    /**
+     * Retrieves a link.
+     *
+     * @route /api/v1/links/{link}
+     * @method GET
+     * @param Request $request
+     * @param Link $link
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function getLink(Request $request, Link $link)
+    {
+        if ($link->user_id !== $request->user()->id) {
+            return $this->belongsToOtherError();
+        }
+
+        return $this->successResponse('Success', [
+            'link' => $link
         ]);
     }
 
     /**
      * Deletes a link.
      *
+     * @route /api/v1/links/{link}
      * @method DELETE
      * @param Link $link
      * @param Request $request
@@ -39,7 +71,7 @@ class LinkController extends APIController
     public function deleteLink(Link $link, Request $request)
     {
         if ($link->user_id !== $request->user()->id) {
-            return $this->errorResponse('Link does not belong to user.', [], 409);
+            return $this->belongsToOtherError();
         }
 
         $link->delete();
@@ -49,28 +81,61 @@ class LinkController extends APIController
     /**
      * Saves a link.
      *
+     * @route /api/v1/links/{link}
      * @method PUT
      * @param Request $request
-     * @param LinkService $service
+     * @param Link $link
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
      */
-    public function saveLink(Request $request, LinkService $service)
+    public function updateLink(Request $request, Link $link)
     {
         $this->validate($request, [
             'url' => 'required'
         ]);
 
-        $link = $service->saveLink(
-            $request->user(),
-            $request->url,
-            $request->title,
-            $request->description
-        );
-        $link->id;
+        if ($link->user_id !== $request->user()->id) {
+            return $this->belongsToOtherError();
+        }
 
-        return $this->successResponse('Success', [
-            'id' => $link->id
+        $link->fill([
+            'title' => $request->title,
+            'description' => $request->description
         ]);
+        $link->save();
+
+        return $this->idBackSuccess($link->id);
+    }
+
+    /**
+     * Creates a new link.
+     *
+     * @route /api/v1/links/new
+     * @method PUT
+     * @param Request $request
+     * @param ModelFactory $factory
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function newLink(Request $request, ModelFactory $factory)
+    {
+        $this->validate($request, [
+            'url' => 'required'
+        ]);
+
+        $link = $this->repository->getLinkForUserByUrl($request->user(), $request->url);
+        if (isset($link)) {
+            return $this->resourceExistsError();
+        }
+
+        /** @var Link $link */
+        $link = $factory->make([
+            'url' => $request->url,
+            'title' => $request->title,
+            'description' => $request->description
+        ], [
+            'user' => $request->user()
+        ]);
+
+        return $this->idBackSuccess($link->id);
     }
 
 }
