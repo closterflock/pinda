f15c7cfacf009ca357fbe39790ea1c2558da39b4
James Spence <Jamesaspence20@gmail.com>
2016-05-18 22:06:58 -0500

    RegistrationController now works

--- app/Http/Controllers/API/RegistrationController.php
+++ app/Http/Controllers/API/RegistrationController.php
@@ -4,36 +4,27 @@
 namespace App\Http\Controllers\API;
 
 
-use App\Models\User;
 use App\Services\UserAndTokenRegistrar;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Http\Request;
-use Laracore\Repository\ModelRepository;
 
 class RegistrationController extends APIController
 {
-
 
     /**
      * Registers a user via the API.
      *
      * @param Request $request
-     * @param ModelRepository $repository
      * @param UserAndTokenRegistrar $registrar
      * @return \App\Models\AuthToken|\Illuminate\Http\Response
      */
-    public function registerUser(Request $request, ModelRepository $repository, UserAndTokenRegistrar $registrar)
+    public function registerUser(Request $request, UserAndTokenRegistrar $registrar)
     {
         $this->validate($request, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'password' => 'required|min:6',
         ]);
-
-        $repository->setModel(User::class);
-        $user = $repository->query()->where('email', '=', $request->email)->first();
-        if (isset($user)) {
-            return $this->resourceExistsError('User already exists for this email.');
-        }
 
         return $registrar->createUserAndAuthTokenFromRequest($request);
     }
