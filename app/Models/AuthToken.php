bc0b675c690798156003dcf37d9149ca8cc8bab7
James Spence <Jamesaspence20@gmail.com>
2016-06-21 20:22:21 -0500

    hiding ip address on authToken

--- app/Models/AuthToken.php
+++ app/Models/AuthToken.php
@@ -11,7 +11,7 @@ class AuthToken extends AbstractModel
 {
     public $guarded = ['id'];
 
-    protected $hidden = ['id', 'created_at', 'updated_at', 'user', 'user_id', 'user_agent'];
+    protected $hidden = ['id', 'created_at', 'updated_at', 'user', 'user_id', 'user_agent', 'ip'];
 
     public function user()
     {
