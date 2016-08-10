34f413d66b09fe1ce4d28eff0c0fe3646309acac
James Spence <Jamesaspence20@gmail.com>
2016-08-10 12:40:09 -0500

    made tag fillable, removed timestamps, added relation

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,10 +6,18 @@ namespace App\Models;
 
 class Tag extends AbstractModel
 {
+    public $fillable = ['name'];
+
+    public $timestamps = false;
 
     public function links()
     {
         return $this->belongsToMany(Link::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
 }
