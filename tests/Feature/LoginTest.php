7ed06823e276f17a7e8bfa1580173517cd60e8f7
James Spence <Jamesaspence20@gmail.com>
2018-07-15 00:43:08 -0500

    login test with stubs

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+
+    public function testNotLoggedInRedirect()
+    {
+        $this->stub();
+    }
+
+    public function testLoginIncorrectEmail()
+    {
+        $this->stub();
+    }
+
+    public function testLoginIncorrectPassword()
+    {
+        $this->stub();
+    }
+
+    public function testLoginSuccess()
+    {
+        $this->stub();
+    }
+
+}
