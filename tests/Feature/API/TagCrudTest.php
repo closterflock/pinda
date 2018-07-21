7a48d0fcafeb4161b5818b22ac16ed54019750c8
James Spence <Jamesaspence20@gmail.com>
2018-07-21 01:21:04 -0500

    stubs for API link/tag CRUD

---
+++ tests/Feature/API/TagCrudTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests\Feature\API;
+
+
+use Tests\TestCase;
+
+class TagCrudTest extends TestCase
+{
+
+    public function testGetTagsSuccess()
+    {
+        $this->stub();
+    }
+
+    public function testCreateTagValidationFailure()
+    {
+        $this->stub();
+    }
+
+    public function testCreateTagSuccess()
+    {
+        $this->stub();
+    }
+
+}
