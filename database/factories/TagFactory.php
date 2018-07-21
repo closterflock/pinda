ede07a646759ddf311d6d84b58da1c9f6d575141
James Spence <Jamesaspence20@gmail.com>
2018-07-21 12:04:29 -0500

    proper tag name column

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -6,7 +6,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Tag::class, function (Faker $faker) {
     return [
-        'tag' => $faker->word
+        'name' => $faker->word
     ];
 });
 
