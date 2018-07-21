b5537316105348fe5df5f012f22002423a739469
James Spence <Jamesaspence20@gmail.com>
2018-07-21 12:57:51 -0500

    parsing carbon object in controller

--- app/Http/Controllers/API/SyncController.php
+++ app/Http/Controllers/API/SyncController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Requests\SyncRequest;
 use App\Services\LocalDataSync;
+use Carbon\Carbon;
 use Illuminate\Http\Response;
 
 class SyncController extends APIController
@@ -21,7 +22,10 @@ class SyncController extends APIController
      */
     public function syncData(SyncRequest $request, LocalDataSync $localDataSync)
     {
-        $timestamp = $request->timestamp;
+        $timestamp = null;
+        if (!is_null($request->get('timestamp', null))) {
+            $timestamp = Carbon::parse($request->timestamp);
+        }
 
         return $this->successResponse('Success', $localDataSync->getDataToSync($request->user(), $timestamp));
     }
