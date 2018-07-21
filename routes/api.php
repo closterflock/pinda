b86553202ec4cca062d1f213ec1bc7eff781a472
James Spence <Jamesaspence20@gmail.com>
2018-07-20 23:15:39 -0500

    moved API routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Middleware\VerifyAPIToken;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => '/api/v1'], function () {
+    Route::post('/login', 'API\CredentialController@login');
+    Route::post('/register', 'API\CredentialController@registerUser');
+
+    Route::group(['middleware' => VerifyAPIToken::class], function () {
+        Route::delete('/logout', 'API\CredentialController@logout');
+
+        Route::group(['prefix' => 'links'], function () {
+            Route::get('/', 'API\LinkController@getLinks');
+            Route::get('/search', 'API\LinkController@getLinksForSearch');
+            Route::post('/new', 'API\LinkController@newLink');
+            Route::get('/{link}', 'API\LinkController@getLink');
+            Route::delete('/{link}', 'API\LinkController@deleteLink');
+            Route::put('/{link}', 'API\LinkController@updateLink');
+        });
+
+        Route::group(['prefix' => 'tags'], function () {
+            Route::get('/', 'API\TagController@getTags');
+            Route::post('/new', 'API\TagController@newTag');
+        });
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -32,29 +32,6 @@ Route::group(['middleware' => 'web'], function () {
         });
     });
 
-    Route::group(['prefix' => '/api/v1'], function () {
-        Route::post('/login', 'API\CredentialController@login');
-        Route::post('/register', 'API\CredentialController@registerUser');
-
-        Route::group(['middleware' => VerifyAPIToken::class], function () {
-            Route::delete('/logout', 'API\CredentialController@logout');
-
-            Route::group(['prefix' => 'links'], function () {
-                Route::get('/', 'API\LinkController@getLinks');
-                Route::get('/search', 'API\LinkController@getLinksForSearch');
-                Route::post('/new', 'API\LinkController@newLink');
-                Route::get('/{link}', 'API\LinkController@getLink');
-                Route::delete('/{link}', 'API\LinkController@deleteLink');
-                Route::put('/{link}', 'API\LinkController@updateLink');
-            });
-
-            Route::group(['prefix' => 'tags'], function () {
-                Route::get('/', 'API\TagController@getTags');
-                Route::post('/new', 'API\TagController@newTag');
-            });
-        });
-    });
-
     Route::post('/token', 'Auth\TokenController@newToken');
 });
 
