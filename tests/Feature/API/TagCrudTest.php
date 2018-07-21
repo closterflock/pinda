c810895f772d8a04b8d6ec54eb7cd3ac451bda18
James Spence <Jamesaspence20@gmail.com>
2018-07-21 01:21:23 -0500

    added missing traits

--- tests/Feature/API/TagCrudTest.php
+++ tests/Feature/API/TagCrudTest.php
@@ -4,10 +4,14 @@
 namespace Tests\Feature\API;
 
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TagCrudTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
 
     public function testGetTagsSuccess()
     {
