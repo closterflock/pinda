397670de565f7b711b27a966d2afa4386ea65603
James Spence <Jamesaspence20@gmail.com>
2018-07-27 18:48:22 -0500

    new api token guard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use App\Models\AuthToken;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laracore\Repository\ModelRepository;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,33 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->registerApiGuard();
+    }
+
+    private function registerApiGuard()
+    {
+        /** @var ModelRepository $repository */
+        $repository = $this->app->make(ModelRepository::class);
+        $repository->setModel(AuthToken::class);
+
+        \Auth::viaRequest('api-token', function (Request $request) use ($repository) {
+            $apiToken = $request->headers->get('X-Auth-Token');
+
+            /** @var AuthToken $authToken */
+            $authToken = $repository
+                ->with('user')
+                ->join('users', 'auth_tokens.user_id', '=', 'users.id')
+                ->where('token', '=', $apiToken)
+                ->first();
+
+            if (is_null($authToken)) {
+                return null;
+            }
+
+            $user = $authToken->user;
+            $user->setAuthToken($authToken);
+
+            return $user;
+        });
     }
 }
