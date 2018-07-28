e071e56193818b0acb20fcab8f7e0637504130dc
James Spence <Jamesaspence20@gmail.com>
2018-07-28 18:59:16 -0500

    using TagRequest for tag API

--- app/Http/Controllers/API/TagController.php
+++ app/Http/Controllers/API/TagController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\API;
 
 
+use App\Http\Requests\TagRequest;
 use App\Models\Repository\TagRepository;
 use App\Services\TagService;
 use Illuminate\Http\Request;
@@ -16,16 +17,12 @@ class TagController extends APIController
      *
      * @method PUT
      * @route /api/v1/tags/new
-     * @param Request $request
+     * @param TagRequest $request
      * @param TagService $service
      * @return \Illuminate\Http\Response
      */
-    public function newTag(Request $request, TagService $service)
+    public function newTag(TagRequest $request, TagService $service)
     {
-        $this->validate($request, [
-            'name' => 'required|string'
-        ]);
-
         $tag = $service->firstOrCreateTag($request->user(), $request->name);
 
         return $this->successResponse('Success', ['id' => $tag->id]);
