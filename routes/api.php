bd18f1e46dcf2bf653f6723c141b249f5d417169
James Spence <Jamesaspence20@gmail.com>
2018-08-06 17:30:27 -0500

    put API namespace on api routes

--- routes/api.php
+++ routes/api.php
@@ -14,31 +14,31 @@ use App\Http\Middleware\VerifyAPIToken;
 */
 
 Route::prefix('v1')->name('api.')->group(function () {
-    Route::post('/login', 'API\CredentialController@login')->name('login');
-    Route::post('/register', 'API\CredentialController@registerUser')->name('register');
+    Route::post('/login', 'CredentialController@login')->name('login');
+    Route::post('/register', 'CredentialController@registerUser')->name('register');
 
     Route::group(['middleware' => 'auth:api'], function () {
-        Route::delete('/logout', 'API\CredentialController@logout')->name('logout');
+        Route::delete('/logout', 'CredentialController@logout')->name('logout');
 
         Route::prefix('links')->name('links.')->group(function () {
-            Route::get('/', 'API\LinkController@getLinks')->name('getLinks');
-            Route::post('/', 'API\LinkController@newLink')->name('store');
-            Route::get('/{link}', 'API\LinkController@getLink')
+            Route::get('/', 'LinkController@getLinks')->name('getLinks');
+            Route::post('/', 'LinkController@newLink')->name('store');
+            Route::get('/{link}', 'LinkController@getLink')
                 ->middleware('can:view,link')
                 ->name('getLink');
-            Route::delete('/{link}', 'API\LinkController@deleteLink')
+            Route::delete('/{link}', 'LinkController@deleteLink')
                 ->middleware('can:delete,link')
                 ->name('delete');
-            Route::put('/{link}', 'API\LinkController@updateLink')
+            Route::put('/{link}', 'LinkController@updateLink')
                 ->middleware('can:update,link')
                 ->name('update');
         });
 
         Route::prefix('tags')->name('tags.')->group(function () {
-            Route::get('/', 'API\TagController@getTags')->name('getTags');
-            Route::post('/', 'API\TagController@newTag')->name('store');
+            Route::get('/', 'TagController@getTags')->name('getTags');
+            Route::post('/', 'TagController@newTag')->name('store');
         });
 
-        Route::get('sync', 'API\SyncController@syncData')->name('sync');
+        Route::get('sync', 'SyncController@syncData')->name('sync');
     });
 });
